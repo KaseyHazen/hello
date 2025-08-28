8d19ed0928d106a3dd691c77b212c2713a645014
Kasey Hazen <khazen5@huskers.unl.edu>
2025-08-28 13:14:27 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Kasey Hazen
+ *
+ * 8/28/2025
+ *
+ * Basic Hello World Program
+ *
+ */
+
+ int main(int agrc, char **argv) {
+
+    printf(Hello I'M Kasey Hazen!\n");
+
+    printf("Major is Computer Science!\n");
+
+    return 0;
+ }
